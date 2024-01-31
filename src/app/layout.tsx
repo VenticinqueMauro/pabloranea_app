59afe9a7174a91ca98860f11419f41c518fda0bb
Venticinque Mauro <mauro.venticinque@hotmail.com>
2024-01-31 11:25:18 -0300

    correccion en metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { Providers } from './providers'
 
 const openSans = Open_Sans({ subsets: ['latin'] })
 
-export const metada: Metadata = {
+export const metadata: Metadata = {
   title: 'Pablo Ranea [Chef & Somm]',
   description: 'Falta crear description'
 }
