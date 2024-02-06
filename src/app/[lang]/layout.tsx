4f6e695651d1dcea7641969b6f80196a61cf53eb
Venticinque Mauro <mauro.venticinque@hotmail.com>
2024-02-06 08:36:58 -0300

    async redirect a /es

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -4,6 +4,7 @@ import { Open_Sans } from 'next/font/google'
 import '../globals.css'
 import { Providers } from '../providers'
 import Link from 'next/link'
+import { redirect } from 'next/navigation'
 
 const openSans = Open_Sans({ subsets: ['latin'] })
 
