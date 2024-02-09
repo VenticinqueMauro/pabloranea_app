4bdaf0fc48fd0718a14863fe55540fc6218a50e4
Venticinque Mauro <mauro.venticinque@hotmail.com>
2024-02-09 11:52:40 -0300

    ajustes padding about pablo

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -4,7 +4,7 @@ export default function About({ dictionary }: any) {
     const { title, button } = dictionary.aboutpablo;
 
     return (
-        <section className="h-screen flex flex-col justify-center scroll-reveal mt-10 bg-black text-white pt-10 lg:pt-20">
+        <section className="h-screen flex flex-col justify-center scroll-reveal mt-10 bg-black text-white pt-10 ">
             <h2 className="text-5xl lg:text-9xl font-bold text-center -tracking-widest lg:-tracking-[16px] letterScroll">{title}</h2>
             <div className="px-6 lg:px-0 text-justify pt-10 pb-5 lg:py-10 max-w-7xl mx-auto">
                 <p className="font-light text-xl lg:text-2xl h-full">
