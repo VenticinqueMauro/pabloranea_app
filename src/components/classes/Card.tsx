f991a4a9fe9d373965dbc202c97d0a82179f0b8a
Venticinque Mauro <mauro.venticinque@hotmail.com>
2024-02-09 06:58:24 -0300

    add: tarjetas en clasess home

---
+++ src/components/classes/Card.tsx
@@ -0,0 +1,11 @@
+
+export default function Card() {
+    return (
+        <div className='cursor-pointer bg-white/70 backdrop-blur py-3 px-3  flex flex-col items-start justify-center '>
+            <span className='text-gray-500 font-semibold'>22.03.04</span>
+            <p className='font-bold'>The Macao University of Science en Macao</p>
+            <p className='text-sm italic'>Macao, China</p>
+            <p>Clase técnica de repulgue</p>
+        </div>
+    )
+}
--- src/components/classes/Classes.tsx
+++ src/components/classes/Classes.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import Image from 'next/image'
 import React from 'react'
+import ContainerCardClasses from './ContainerCard.classes'
 
 export default function Classes({ dictionary }: any) {
 
@@ -20,18 +21,7 @@ export default function Classes({ dictionary }: any) {
                     }}
                 />
             </div>
-            <div className='hidden absolute bottom-5 right-3 px-1 bg-white lg:flex flex-col items-start justify-center gap-4 h-[400px] overflow-y-auto'>
-                {
-                    Array.from({ length: 5 }, (_, index) => (
-                        <div key={index} className='cursor-pointer px-3 border-b border-dashed py-5 flex flex-col items-start justify-center '>
-                            <span className='text-gray-500 font-semibold'>22.03.04</span>
-                            <p className='font-bold'>The Macao University of Science en Macao</p>
-                            <p className='text-sm italic'>Macao, China</p>
-                            <p>Clase técnica de repulgue</p>
-                        </div>
-                    ))
-                }
-            </div>
+            <ContainerCardClasses />
             <div className="absolute bottom-32 left-0 px-6  lg:px-20 max-w-md text-justify text-white scroll-parallax">
                 <h2 className="text-6xl lg:text-7xl font-bold text-shadow -tracking-widest">{title}</h2>
                 <h3 className="text-sm md:text-base pt-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta illo alias impedit corporis quod cupiditate excepturi eveniet eaque, asperiores quidem illum quae ratione expedita voluptatem nihil molestiae laudantium inventore pariatur ipsum assumenda rem voluptates aliquam? Libero amet nihil iusto, cumque, quidem a aspernatur tempora ratione voluptatibus assumenda debitis et ducimus?</h3>
