689357bcbee3cc2eff416e2002352aba80dcb7fa
Venticinque Mauro <mauro.venticinque@hotmail.com>
2024-02-15 19:51:40 -0300

    agregado de scroll en classes y calendar home

--- src/components/classes/Card.tsx
+++ src/components/classes/Card.tsx
@@ -1,7 +1,7 @@
 
 export default function Card() {
     return (
-        <div className='cursor-pointer bg-white backdrop-blur py-3 px-3  flex flex-col items-start justify-center shadow-md'>
+        <div className='cursor-pointer bg-white backdrop-blur py-3 px-3  flex flex-col items-start justify-center shadow-md '>
             <span className='text-gray-500 font-semibold'>22.03.04</span>
             <p className='font-bold'>The Macao University of Science en Macao</p>
             <p className='text-sm italic light'>Macao, China</p>
