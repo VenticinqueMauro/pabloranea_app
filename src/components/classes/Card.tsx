2f82439111b4475e4a17bf529dc967318388722c
Venticinque Mauro <mauro.venticinque@hotmail.com>
2024-02-17 11:13:54 -0300

    hover card classes

--- src/components/classes/Card.tsx
+++ src/components/classes/Card.tsx
@@ -1,7 +1,7 @@
 
 export default function Card() {
     return (
-        <div className='cursor-pointer bg-black/10 backdrop-blur mt-2 py-3 px-3  flex flex-col items-start justify-center shadow-md relative overflow-x-hidden'>
+        <div className='lg:hover:bg-black/50 transition-colors duration-200 cursor-pointer bg-black/10 backdrop-blur mt-2 py-3 px-3  flex flex-col items-start justify-center shadow-md relative overflow-x-hidden'>
             <div className='absolute top-0 flex w-full justify-center'>
                 <div className='left-0 h-[1px] animate-border-width rounded-full bg-gradient-to-r from-[rgba(17,17,17,0)] via-white to-[rgba(17,17,17,0)] transition-all duration-1000' />
             </div>
