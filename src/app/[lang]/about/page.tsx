a56bad9dbf812d48ee540737c2e8bb8a1a8b275b
Venticinque Mauro <mauro.venticinque@hotmail.com>
2024-02-20 17:43:26 -0300

    section4 en about pablo

--- src/app/[lang]/about/page.tsx
+++ src/app/[lang]/about/page.tsx
@@ -14,7 +14,7 @@ export default function page() {
                 <div className="h-[800px] block bg-gray w-full bg-gray-400"></div>
             </div>
             {/* SECTION 3 */}
-            <div className="h-screen">
+            <div >
                 <div className="flex justify-between px-3 lg:px-20 py-16 bg-background text-foreground dark">
                     <h2 className="uppercase text-2xl font-bold w-full col-span-12 lg:col-span-6">la tierra del sol y el buen vino</h2>
                     <h3 className="text-lg lg:text-2xl text-justify w-full" style={{ lineHeight: 1.2 }}>Su fuerte conexión con Mendoza y el mundo vitivinícola, y su
@@ -46,7 +46,48 @@ export default function page() {
                 </div>
             </div>
             {/* SECTION 4 */}
-
+            <div className="bg-background text-foreground light px-3 lg:px-20">
+                <div className="flex justify-between gap-10">
+                    <Image radius="none" className="object-cover " src="/partners/partners.jpg" alt="test2" />
+                    <Image radius="none" className="object-cover " src="/partners/partners.jpg" alt="test2" />
+                </div>
+                <div className="flex justify-between py-20 gap-10">
+                    <h2 className="uppercase text-2xl font-bold w-full col-span-12 lg:col-span-6">EMBAJADORDELVINOARGENTINO</h2>
+                    <div className="w-full space-y-10">
+                        <h3 className="text-lg lg:text-2xl text-justify" style={{ lineHeight: 1.2 }}>
+                            Reconocidas bodegas de Mendoza como <b>El Enemigo, Catena
+                            Zapata, Susana Balbo, Trivento, Rutini, Nieto Senetiner,
+                            AchavalFerrer, AWI Wines</b> así como también <b>Colomé</b> de Salta,
+                            <b>Otronia</b> de la Patagonia Argentina confían en <b>Ranea</b> como
+                            comunicador y embajador para presentar sus más exclusivas
+                            etiquetas en sus cenas nómades en el extranjero y de esta
+                            manera,llegar a nuevos consumidores.
+                        </h3>
+                    </div>
+                </div>
+                <div className="flex justify-between gap-10">
+                    <Image radius="none" className="object-cover " src="/partners/partners.jpg" alt="test2" />
+                    <Image radius="none" className="object-cover " src="/partners/partners.jpg" alt="test2" />
+                </div>
+                <div className="flex py-20 justify-between gap-36">
+                    <div className="uppercase font-extrabold text-2xl flex-shrink-0" style={{ lineHeight: 1.2 }}>
+                        <p>new york</p>
+                        <p>san francisco</p>
+                        <p>houston</p>
+                        <p>denver</p>
+                        <p>lima</p>
+                        <p>canadá</p>
+                        <p>hong kong</p>
+                    </div>
+                    <h2 className="text-2xl flex-shrink text-justify">
+                        A lo largo de estos años, Pablo Ranea ha sabido construir un nombre propio en la gastronomía no
+                        sólo en Argentina sino también en el exterior. New York, San Francisco, Houston, y Denver enUSA;
+                        como tambien Perú, Canadá, y Hong Kong son algunas de las regiones donde plantó bandera
+                        creando una comunidad foodie fiel de comensales aventureros que siempre están atentos a todas
+                        lasnovedadessobrevinosargentinosydeleitarsussentidosconsusfabulososplatos.
+                    </h2>
+                </div>
+            </div>
         </section>
     )
 }
