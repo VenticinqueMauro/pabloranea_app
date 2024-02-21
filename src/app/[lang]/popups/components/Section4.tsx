233721dcbb78d05cf7c4063e770f14c17d6e3d7f
Venticinque Mauro <mauro.venticinque@hotmail.com>
2024-02-21 07:44:35 -0300

    responsive en popups page

--- src/app/[lang]/popups/components/Section4.tsx
+++ src/app/[lang]/popups/components/Section4.tsx
@@ -3,15 +3,15 @@ import React from 'react'
 
 export default function Section4() {
     return (
-        <div className="bg-background text-foreground light px-20 py-16 grid grid-cols-12 grid-rows-3 gap-10">
-            <div className="col-span-6 row-span-2">
-                <div className="flex items-center gap-10">
-                    <h2 className="text-9xl font-extrabold">5</h2>
-                    <h3 className="text-3xl uppercase font-extrabold">eventos destacados</h3>
+        <div className="bg-background text-foreground light px-3 lg:px-20 py-16 grid grid-cols-12 lg:grid-rows-3 gap-10">
+            <div className="col-span-12 lg:col-span-6 lg:row-span-2">
+                <div className="flex items-center gap-5 lg:gap-10">
+                    <h2 className="text-7xl lg:text-9xl font-extrabold">5</h2>
+                    <h3 className="text-xl lg:text-3xl uppercase font-extrabold">eventos destacados</h3>
                 </div>
             </div>
-            <div className="row-span-1 col-span-6">
-                <div className="text-3xl font-bold pt-10">
+            <div className="col-span-12 lg:row-span-1 lg:col-span-6">
+                <div className="text-xl lg:text-3xl font-bold lg:pt-10">
                     Pop Up Dinner para el Festival Argentino en el American Club
                     HONGKONG,AÑO
                     <br /><br />
@@ -28,10 +28,10 @@ export default function Section4() {
                     LASVEGASNEVADA,AÑO
                 </div>
             </div>
-            <div className="col-span-6 row-span-1 row-start-3 flex justify-center items-end">
+            <div className="col-span-12 lg:col-span-6 lg:row-span-1 lg:row-start-3 flex justify-center lg:items-end">
                 <Image radius="none" className="object-cover w-full h-full" src="/popups/img2.jpg" alt="popups dinners" width={720} height={600} />
             </div>
-            <div className="col-span-6 min-h-full row-span-2 row-start-2 flex items-end">
+            <div className="hidden lg:col-span-6 min-h-full lg:row-span-2 lg:row-start-2 lg:flex items-end">
                 <Image radius="none" className="object-cover w-full h-full" src="/popups/vertical2.jpg" alt="popups dinners" width={720} height={1079} />
             </div>
         </div>
--- src/app/[lang]/popups/components/Section5.jsx
+++ src/app/[lang]/popups/components/Section5.jsx
@@ -9,63 +9,63 @@ export default function Section5() {
     const [isEnter, setIsEnter] = useState(false);
 
     return (
-        <div className="bg-background text-foreground dark px-20 pt-16 pb-20 min-h-screen">
+        <div className="bg-background text-foreground dark px-3 lg:px-20 pt-16 pb-20 min-h-screen">
             <ScrollTrigger onEnter={() => setIsEnter(true)} onExit={() => setIsEnter(false)}>
-                <div className="flex items-center gap-10">
-                    <div className="relative w-full flex flex-col justify-between items-start h-screen">
+                <div className="flex flex-col lg:flex-row items-center gap-5 lg:gap-10">
+                    <div className="relative w-full flex flex-col justify-around lg:justify-between items-center lg:items-start h-screen">
                         <div className="relative">
                             <h2 className="text-[250px] font-medium">
-                                {isEnter && <CountUp start={0} end={40} duration={5} />}
+                                {isEnter && <CountUp start={0} end={40} duration={15} />}
                             </h2>
                             <h3 className="uppercase absolute bottom-8 text-xl left-10">ciudades recorridas</h3>
                         </div>
                         <div className="grid grid-cols-2 max-w-md">
                             <div className="col-span-2 flex items-center gap-5 min-w-full">
                                 <p className="text-7xl text-white w-[50%] text-end">
-                                    {isEnter && <CountUp start={0} end={4} duration={5} />}
+                                    {isEnter && <CountUp start={0} end={4} duration={10} />}
                                 </p>
                                 <p className="uppercase w-[50%] text-start">paises</p>
                             </div>
                             <div className="col-span-2 flex items-center gap-5 min-w-full">
                                 <p className="text-7xl text-white w-full text-end">
-                                    {isEnter && <CountUp start={0} end={1500} decimals={0} separator="" duration={5} />}
+                                    {isEnter && <CountUp start={0} end={1500} decimals={0} separator="" duration={10} />}
                                 </p>
                                 <p className="uppercase w-full text-start">comensales</p>
                             </div>
                             <div className="col-span-2 flex items-center gap-5 min-w-full">
                                 <p className="text-7xl text-white w-full text-end">
-                                    {isEnter && <CountUp start={0} end={50} duration={5} />}
+                                    {isEnter && <CountUp start={0} end={50} duration={10} />}
                                 </p>
                                 <p className="uppercase w-full text-start">litros de chimichurri</p>
                             </div>
                             <div className="col-span-2 flex items-center gap-5 min-w-full">
                                 <p className="text-7xl text-white w-full text-end">
-                                    {isEnter && <CountUp start={0} end={180} duration={5} />}
+                                    {isEnter && <CountUp start={0} end={180} duration={10} />}
                                 </p>
                                 <p className="uppercase w-full text-start">kilos de dulce de leche</p>
                             </div>
                             <div className="col-span-2 flex items-center gap-5 min-w-full">
                                 <p className="text-7xl text-white w-full text-end">
-                                    {isEnter && <CountUp start={0} end={1400} decimals={0} separator="" duration={5} />}
+                                    {isEnter && <CountUp start={0} end={1400} decimals={0} separator="" duration={10} />}
                                 </p>
                                 <p className="uppercase w-full text-start">botellas de vino</p>
                             </div>
                         </div>
                     </div>
-                    <div className="w-full flex flex-col justify-between min-h-full gap-28">
+                    <div className="w-full flex flex-col justify-between min-h-full gap-10 lg:gap-28">
                         <Image radius="none" className="object-cover w-full h-full" src="/popups/vertical3.jpg" alt="popups dinners" width={900} height={1350} />
-                        <div>
-                            <div className="flex items-center gap-5 justify-start w-fit max-w-lg">
+                        <div className='flex flex-col items-center lg:items-start gap-5'>
+                            <div className="flex flex-col lg:flex-row items-center gap-5 justify-start w-fit max-w-lg">
                                 <p className="text-7xl text-white ">
-                                    {isEnter && <CountUp start={0} end={60} duration={5} />}
+                                    {isEnter && <CountUp start={0} end={60} duration={10} />}
                                 </p>
                                 <p className="uppercase max-w-sm ">60 pop up dinners al año.</p>
                             </div>
-                            <div className="flex items-center gap-5 justify-start w-fit max-w-lg">
+                            <div className="flex flex-col lg:flex-row items-center gap-5 justify-start w-fit max-w-lg">
                                 <p className="text-5xl text-white ">
                                     {isEnter && (
                                         <span>
-                                            <CountUp start={0} end={16} duration={5} /> a <CountUp start={0} end={25} duration={5} />
+                                            <CountUp start={0} end={16} duration={10} /> a <CountUp start={0} end={25} duration={10} />
                                         </span>
                                     )}
                                 </p>
