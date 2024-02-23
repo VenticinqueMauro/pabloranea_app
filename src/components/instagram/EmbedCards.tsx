5c9cb595e8b2c85eb6630591f267f325a138c374
Venticinque Mauro <mauro.venticinque@hotmail.com>
2024-02-23 11:53:07 -0300

    componentizacion de cards instagram

---
+++ src/components/instagram/EmbedCards.tsx
@@ -0,0 +1,11 @@
+import { InstagramEmbed } from "react-social-media-embed";
+
+export default function EmbedCards() {
+    return (
+        <div className='flex flex-col lg:flex-row gap-4'>
+            <InstagramEmbed url="https://www.instagram.com/reel/C2zuz0tLu81/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" width={328} height={615} />
+            <InstagramEmbed url="https://www.instagram.com/p/C1c60-LOafQ/" width={328} height={615} />
+            <InstagramEmbed url="https://www.instagram.com/reel/C2SyXnyOrMm/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" width={328} height={615} />
+        </div>
+    )
+}
--- src/components/instagram/Instagram.tsx
+++ src/components/instagram/Instagram.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { InstagramEmbed } from 'react-social-media-embed';
+import EmbedCards from './EmbedCards';
 
 export const Instagram = () => {
 
@@ -8,11 +9,7 @@ export const Instagram = () => {
         <section id='community' className='flex justify-center'>
             <div className='max-w-7xl  gap-5 px-6 lg:px-0  py-10'>
                 <h2 className="text-5xl lg:text-8xl font-bold text-shadow -tracking-[4px] lg:-tracking-[6px] py-20 text-center" style={{ lineHeight: 0.8 }}>Sigue nuestro tour 2024</h2>
-                <div className='flex flex-col lg:flex-row gap-4'>
-                    <InstagramEmbed url="https://www.instagram.com/reel/C2zuz0tLu81/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" width={328} height={615} />
-                    <InstagramEmbed url="https://www.instagram.com/p/C1c60-LOafQ/" width={328} height={615} />
-                    <InstagramEmbed url="https://www.instagram.com/reel/C2SyXnyOrMm/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" width={328} height={615} />
-                </div>
+                <EmbedCards />
             </div>
         </section>
     )
