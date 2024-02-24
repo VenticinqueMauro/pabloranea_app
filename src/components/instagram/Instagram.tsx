817d9f631adb35baa6efe3c5507567e5ac95e78e
Venticinque Mauro <mauro.venticinque@hotmail.com>
2024-02-24 15:12:31 -0300

    cambios en layout instagram

--- src/components/instagram/Instagram.tsx
+++ src/components/instagram/Instagram.tsx
@@ -1,13 +1,14 @@
 'use client'
 
-import { InstagramEmbed } from 'react-social-media-embed';
-import EmbedCards from './EmbedCards';
+import dynamic from "next/dynamic";
+
+const EmbedCards = dynamic(() => import('./EmbedCards'), { ssr: false });
 
 export const Instagram = () => {
 
     return (
-        <section id='community' className='flex justify-center'>
-            <div className='max-w-7xl  gap-5 px-6 lg:px-0  py-10'>
+        <section id='community' className='flex justify-center items-center px-3 lg:px-0'>
+            <div className='max-w-7xl py-10'>
                 <h2 className="text-5xl lg:text-8xl font-bold text-shadow -tracking-[4px] lg:-tracking-[6px] py-20 text-center" style={{ lineHeight: 0.8 }}>Sigue nuestro tour 2024</h2>
                 <EmbedCards />
             </div>
