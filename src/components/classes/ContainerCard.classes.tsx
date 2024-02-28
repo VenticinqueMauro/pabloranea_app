f3d6f3a24d76d8fc1493c15266ee97ede0a98cbf
Venticinque Mauro <mauro.venticinque@hotmail.com>
2024-02-28 11:01:19 -0300

    cards classes en home para mobile

--- src/components/classes/ContainerCard.classes.tsx
+++ src/components/classes/ContainerCard.classes.tsx
@@ -2,14 +2,17 @@ import Card from "./Card";
 
 export default function ContainerCardClasses() {
     return (
-        <div className='hidden absolute bottom-5 right-3 px-1 lg:flex flex-col items-start justify-center gap-2 max-h-[400px] overflow-y-auto overscroll-auto focus:overscroll-contain container-classes snap-y snap-mandatory snap-start z-10' >
-            <div className='mt-24 space-y-2'>
-                {
-                    Array.from({ length: 4 }, (_, index) => (
-                        <Card key={index} />
-                    ))
-                }
+        <>
+            <div className='absolute bottom-5 left-1/2 -translate-x-1/2 ml-auto sm:-translate-x-0 w-full px-3 sm:w-fit sm:right-3 sm:px-1 lg:flex flex-col items-start justify-center gap-2 max-h-[300px] lg:max-h-[400px] overflow-y-auto overscroll-auto focus:overscroll-contain container-classes z-10 hide-scrollbar' style={{ WebkitOverflowScrolling: 'touch' }}>
+
+                <div className='mt-24 space-y-2 '>
+                    {
+                        Array.from({ length: 4 }, (_, index) => (
+                            <Card key={index} />
+                        ))
+                    }
+                </div>
             </div>
-        </div>
+        </>
     )
 }
