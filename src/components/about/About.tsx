e9679db08a276d6ece0e8c4e032a8967d1d40bd5
Venticinque Mauro <mauro.venticinque@hotmail.com>
2024-02-28 10:10:27 -0300

    alineacion en texto about home

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -14,7 +14,7 @@ export default function About({ dictionary, lang }: Props) {
         <section id='about' className="h-screen flex flex-col justify-center scroll-reveal mt-10 bg-black text-white pt-10 ">
             <h2 className="text-5xl lg:text-9xl font-bold text-center -tracking-widest lg:-tracking-[10px] letterScroll">{title}</h2>
             <div className="px-6 lg:px-0 text-justify pt-10 pb-5 lg:py-10 max-w-7xl mx-auto">
-                <p className="font-light text-base lg:text-2xl h-full light">
+                <p className="font-light text-start lg:text-justify text-base lg:text-2xl h-full light">
                     <span>
                         {description}
                     </span>
