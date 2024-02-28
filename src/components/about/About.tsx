0097ba3a2ae31484ab25d7e387362c4d8391def6
Venticinque Mauro <mauro.venticinque@hotmail.com>
2024-02-28 10:08:58 -0300

    modificacion en mobile de tamano de texto en about home

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -14,11 +14,11 @@ export default function About({ dictionary, lang }: Props) {
         <section id='about' className="h-screen flex flex-col justify-center scroll-reveal mt-10 bg-black text-white pt-10 ">
             <h2 className="text-5xl lg:text-9xl font-bold text-center -tracking-widest lg:-tracking-[10px] letterScroll">{title}</h2>
             <div className="px-6 lg:px-0 text-justify pt-10 pb-5 lg:py-10 max-w-7xl mx-auto">
-                <p className="font-light text-xl lg:text-2xl h-full light">
+                <p className="font-light text-base lg:text-2xl h-full light">
                     <span>
                         {description}
                     </span>
-                    <Link href={`/${lang}/about`} className="flex justify-between gap-10 pt-10  w-fit font-bold ms-auto">
+                    <Link href={`/${lang}/about`} className="flex justify-between gap-10 pt-10 text-base lg:text-lg  w-fit font-bold ms-auto">
                         <span>
                             {`>>>>>>`}
                         </span>
