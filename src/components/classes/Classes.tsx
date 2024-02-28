799c608fd47e40c7e90df419ac5ec82aafddf590
Venticinque Mauro <mauro.venticinque@hotmail.com>
2024-02-28 10:15:08 -0300

    ajustes en clases y partners titulo home

--- src/components/classes/Classes.tsx
+++ src/components/classes/Classes.tsx
@@ -10,7 +10,7 @@ export default function Classes({ dictionary }: any) {
             <ContainerCardClasses />
             <div className={`imageContainer-classes`}>
                 <div className={`imageOverlay-classes`} />
-                <div className={`contentContainer absolute top-0 left-0 px-6  lg:px-20 lg:py-10 max-w-lg text-justify text-white scroll-parallax`}>
+                <div className={`contentContainer absolute top-0 left-0 px-6  lg:px-20 py-10 lg:py-16 max-w-lg text-justify text-white scroll-parallax`}>
                     <h2 className="text-6xl lg:text-7xl font-bold text-shadow -tracking-[3px] lg:-tracking-[3px]" style={{ lineHeight: 0.8 }}>{title}</h2>
                     <h3 className="text-sm md:text-base pt-5 text-start" style={{ lineHeight: 1.2 }}>{description}</h3>
                 </div>
--- src/components/partners/Partners.tsx
+++ src/components/partners/Partners.tsx
@@ -9,7 +9,7 @@ export default function Partners({ dictionary, lang }: any) {
         <section id='partners' className="min-h-screen relative overflow-x-clip  ">
             <div className={`imageContainer`}>
                 <div className={`imageOverlay-partners`} />
-                <div className={`contentContainer absolute top-0 left-0 px-6  lg:px-20 lg:py-10 max-w-lg text-justify text-white scroll-parallax`}>
+                <div className={`contentContainer absolute top-0 left-0 px-6  lg:px-20 py-10 lg:py-16 max-w-lg text-justify text-white scroll-parallax`}>
                     <h2 className={`${title1.startsWith('The') ? 'text-5xl lg:text-[65px]' : 'text-6xl lg:text-7xl'}  font-bold text-shadow text-start -tracking-[3px] lg:-tracking-[4px]`} style={{ lineHeight: 0.9 }}>{title1}<br />{title2}</h2>
                     <h3 className="text-sm md:text-base pt-5 text-start" style={{ lineHeight: 1.2 }}>{description}</h3>
                     <a href={`/${lang}/partners`} className="flex justify-between w-fit gap-10 font-bold pt-10">
