d74dd322047740ba137d4d09871ed581be94a5ed
Venticinque Mauro <mauro.venticinque@hotmail.com>
2024-02-29 08:22:21 -0300

    portadas popup y about full width

--- src/app/[lang]/about/components/Section1.tsx
+++ src/app/[lang]/about/components/Section1.tsx
@@ -2,7 +2,7 @@ import { Image } from "@nextui-org/react";
 
 export default function Section1() {
     return (
-        <div className="relative lg:px-20">
+        <div className="relative">
             <Image className="aspect-[3.3/1]" radius="none" src="/aboutpablo/portadapage.png" alt="portada popups" width={2760} height={825} />
         </div>
     )
--- src/app/[lang]/popup/components/Section1.tsx
+++ src/app/[lang]/popup/components/Section1.tsx
@@ -2,7 +2,7 @@ import { Image } from "@nextui-org/react";
 
 export default function Section1() {
     return (
-        <div className="relative lg:px-20">
+        <div className="relative">
             <Image className="lg:hidden aspect-[3.3/1]" radius="none" src="/popups/portadapage.png" alt="portada popups" width={2760} height={825}/>
             <Image className="hidden lg:block aspect-[3.3/1]" radius="none" src="/popups/portadapage.png" alt="portada popups" width={2760} height={825} />
         </div>
