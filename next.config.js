316edf057a3d83cd96cdaf81de3c1657945d1d58
Venticinque Mauro <mauro.venticinque@hotmail.com>
2024-03-27 18:28:27 -0300

    change la web se inicia en /en, y modificacion de filtros en calendario

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     return [
       {
         source: '/',
-        destination: '/es',
+        destination: '/en',
         permanent: true,
       },
     ]
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -11,18 +11,37 @@ export async function getStays(): Promise<Stay[] | undefined> {
 
         const stays: Stay[] = staysData.map((stayData) => ({
             ...stayData,
-            startDate: new Date(stayData.startDate)
+            startDate: new Date(stayData.startDate),
+            endDate: new Date(stayData.endDate)
         }));
 
+        const currentDate = new Date();
+
         stays.sort((a, b) => {
-            const fechaActual = new Date();
-            const diffA = Math.abs(a.startDate.getTime() - fechaActual.getTime());
-            const diffB = Math.abs(b.startDate.getTime() - fechaActual.getTime());
-            return diffA - diffB;
+            // Si la fecha de finalización de la estadía A aún no ha pasado al día de hoy
+            // y la fecha de finalización de la estadía B ya pasó, A debe aparecer primero.
+            if (a.endDate >= currentDate && b.endDate < currentDate) {
+                return -1;
+            }
+            // Si la fecha de finalización de la estadía A ya pasó
+            // y la fecha de finalización de la estadía B aún no ha pasado, A debe aparecer después.
+            if (a.endDate < currentDate && b.endDate >= currentDate) {
+                return 1;
+            }
+            // Si ambas estadías aún no han finalizado, ordenar por cercanía a la fecha de inicio.
+            if (a.endDate >= currentDate && b.endDate >= currentDate) {
+                return a.startDate.getTime() - b.startDate.getTime();
+            }
+            // Si ambas estadías ya han finalizado, ordenar por cercanía a la fecha de finalización.
+            return a.endDate.getTime() - b.endDate.getTime();
         });
 
-        return stays;
+        // Filtrar estadías cuya fecha de finalización aún no ha pasado
+        const activeStays = stays.filter(stay => stay.endDate >= currentDate);
+
+        return activeStays;
     } else {
         return undefined;
     }
 }
+
