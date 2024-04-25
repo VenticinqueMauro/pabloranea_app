15646fc23f46e84b6e2d262cb5747698e3494cf4
Venticinque Mauro <mauro.venticinque@hotmail.com>
2024-04-25 14:29:16 -0300

    add clase con enlace de compra de entrada

--- src/components/classes/Card.tsx
+++ src/components/classes/Card.tsx
@@ -1,16 +1,36 @@
+import Link from "next/link"
 import { Class } from "./ContainerCard.classes"
+import { ExternalLink } from 'lucide-react'
 
 
 
 export default function Card({ classItem }: { classItem: Class }) {
     return (
-        <div className='lg:hover:bg-black/50 transition-colors duration-200 cursor-pointer bg-black/10 backdrop-blur mt-2 py-3 px-3  flex flex-col items-start justify-center shadow-md relative overflow-x-hidden text-sm lg:text-base lg:max-w-[400px]'>
-            <div className='absolute top-0 flex w-full justify-center'>
-                <div className='left-0 h-[1px] animate-border-width rounded-full bg-gradient-to-r from-[rgba(17,17,17,0)] via-white to-[rgba(17,17,17,0)] transition-all duration-1000' />
-            </div>
-            <span className='text-gray-300 font-semibold light'>{classItem.date}</span>
-            <p className='font-bold text-white '>{classItem.description}</p>
-            <p className='text-xs lg:text-sm italic light text-gray-200'>{classItem.location}</p>
-        </div>
+        <>
+            {
+                classItem?.link ?
+                    <Link href={classItem.link} className='lg:hover:bg-black/50 transition-colors duration-200 cursor-pointer bg-black/10 backdrop-blur mt-2 py-3 px-3  flex flex-col items-start justify-center shadow-md relative overflow-x-hidden text-sm lg:text-base lg:max-w-[400px]'>
+                        <div className='absolute top-0 flex w-full justify-center' >
+                            <div className='left-0 h-[1px] animate-border-width rounded-full bg-gradient-to-r from-[rgba(17,17,17,0)] via-white to-[rgba(17,17,17,0)] transition-all duration-1000' />
+                        </div>
+                        <span className='text-gray-300 font-semibold light'>{classItem.date}</span>
+                        <p className='font-bold text-white '>{classItem.description}</p>
+                        <p className='text-xs lg:text-sm italic light text-gray-200'>{classItem.location}</p>
+                        <div className='absolute top-1 right-1 text-white hover:text-blue-500 flex items-center'>
+                            <p className='mr-2 text-sm cursor-pointer '>Buy tickets</p>
+                            <ExternalLink size={16} />
+                        </div>
+                    </Link >
+                    :
+                    <div className='lg:hover:bg-black/50 transition-colors duration-200 cursor-pointer bg-black/10 backdrop-blur mt-2 py-3 px-3  flex flex-col items-start justify-center shadow-md relative overflow-x-hidden text-sm lg:text-base lg:max-w-[400px]'>
+                        <div className='absolute top-0 flex w-full justify-center'>
+                            <div className='left-0 h-[1px] animate-border-width rounded-full bg-gradient-to-r from-[rgba(17,17,17,0)] via-white to-[rgba(17,17,17,0)] transition-all duration-1000' />
+                        </div>
+                        <span className='text-gray-300 font-semibold light'>{classItem.date}</span>
+                        <p className='font-bold text-white '>{classItem.description}</p>
+                        <p className='text-xs lg:text-sm italic light text-gray-200'>{classItem.location}</p>
+                    </div>
+            }
+        </>
     )
 }
--- src/components/classes/ContainerCard.classes.tsx
+++ src/components/classes/ContainerCard.classes.tsx
@@ -4,18 +4,25 @@ export interface Class {
     date: string;
     location: string;
     description: string;
+    link?: string;
 }
 
 const classes: Class[] = [
     {
+        date: 'Tuesday, May 7',
+        location: 'Central Market Cooking School, Houston - Texas', 
+        description: 'Food & Wine from South America with Chef Pablo Ranea',
+        link: 'https://www.eventbrite.com/e/food-wine-from-south-america-with-chef-pablo-ranea-tickets-887704507077?aff=oddtdtcreator'
+    },
+    {
         date: '29.04 – 02.06',
         location: 'Houston – San Antonio, Texas', 
-        description: 'May 8th Food & Wine pairing class at Central Market in Houston'
+        description: 'May 8th Food & Wine pairing class at Central Market in Houston',
     },
     {
         date: '03.06 – 22.07',
         location: 'New York – Washington DC – Asbury Pk, East Coast', 
-        description: 'June 4th Argentinian wine class at ICE Institute of Culinary Education in Battery Park New York'
+        description: 'June 4th Argentinian wine class at ICE Institute of Culinary Education in Battery Park New York',
     }
 ];
 
@@ -25,7 +32,7 @@ export default function ContainerCardClasses() {
         <>
             <div className='absolute bottom-5 left-1/2 -translate-x-1/2 ml-auto sm:-translate-x-0 w-full px-3 sm:w-fit sm:right-3 sm:px-1 lg:flex flex-col items-start justify-center gap-2 max-h-[250px] lg:max-h-[400px] overflow-y-auto overscroll-auto focus:overscroll-contain container-classes z-10 hide-scrollbar' style={{ WebkitOverflowScrolling: 'touch' }}>
 
-                <div className='mt-24 space-y-2 '>
+                <div className='mt-24 md:mt-0 space-y-2 '>
                     {
                         classes.map((classItem) => (
                             <Card key={classItem.date} classItem={classItem} />
