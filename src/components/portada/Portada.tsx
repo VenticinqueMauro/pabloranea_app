d0115d3a17298471811c99a4fe1e637242c5a455
Venticinque Mauro <mauro.venticinque@hotmail.com>
2024-05-22 08:02:42 -0300

    change cambio de imagen en susanabalbo en portada version mobile y agregado de loop al slider

--- src/components/portada/Portada.tsx
+++ src/components/portada/Portada.tsx
@@ -4,9 +4,27 @@ import Autoplay from 'embla-carousel-autoplay';
 import useEmblaCarousel from 'embla-carousel-react';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
+import { useEffect, useState, useRef } from 'react';
 
 export default function Portada() {
-    const [emblaRef] = useEmblaCarousel({ loop: false }, [Autoplay({ delay: 2000 })])
+    const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay({ delay: 2000 })]);
+    const [isMobile, setIsMobile] = useState(false);
+    const ref = useRef(null);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.matchMedia('(max-width: 768px)').matches);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        // Initial check
+        handleResize();
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
 
     return (
         <motion.div
@@ -17,20 +35,26 @@ export default function Portada() {
         >
             <div className="embla__container">
                 {
-                    Array.from({ length: 7 }, (_, index) => (
-                        <div key={`slide${index}`} className="embla__slide">
-                            <Image
-                                src={`/portada/slide${index + 1}.jpg`}
-                                width={1980}
-                                height={1114}
-                                alt='portada'
-                                className="object-cover w-full h-screen sm:h-screen"
-                                // style={{ objectPosition: 'center bottom' }}
-                            />
-                        </div>
-                    ))
+                    Array.from({ length: 7 }, (_, index) => {
+                        const slideNumber = index + 1;
+                        const src = slideNumber === 4 && isMobile
+                            ? `/portada/slide${slideNumber}-mobile.jpg`
+                            : `/portada/slide${slideNumber}.jpg`;
+
+                        return (
+                            <div key={`slide${index}`} className="embla__slide">
+                                <Image
+                                    src={src}
+                                    width={1980}
+                                    height={1114}
+                                    alt='portada'
+                                    className="object-cover  w-full h-screen sm:h-screen"
+                                />
+                            </div>
+                        );
+                    })
                 }
             </div>
         </motion.div>
-    )
+    );
 }
