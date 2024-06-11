0e4b107ae42d19a29523121c112e65c8f744f94e
Venticinque Mauro <102001296+VenticinqueMauro@users.noreply.github.com>
2024-06-11 08:05:00 -0300

    change instagrams embedcards urls

--- src/components/instagram/EmbedCards.tsx
+++ src/components/instagram/EmbedCards.tsx
@@ -3,9 +3,9 @@ import { InstagramEmbed } from "react-social-media-embed";
 export default function EmbedCards() {
     return (
         <div className='flex flex-col items-center lg:justify-center lg:flex-row gap-4'>
-            <InstagramEmbed url="https://www.instagram.com/reel/C2zuz0tLu81/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" width={328} height={615} />
-            <InstagramEmbed url="https://www.instagram.com/p/C1c60-LOafQ/" width={328} height={615} />
-            <InstagramEmbed url="https://www.instagram.com/reel/C2SyXnyOrMm/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" width={328} height={615} />
+            <InstagramEmbed url="https://www.instagram.com/reel/C5o4wHIuZaB/" width={328} height={615} />
+            <InstagramEmbed url="https://www.instagram.com/reel/C7mqPkVOHXi/" width={328} height={615} />
+            <InstagramEmbed url="https://www.instagram.com/reel/C7h1kanu3KL/" width={328} height={615} />
         </div>
     )
 }
