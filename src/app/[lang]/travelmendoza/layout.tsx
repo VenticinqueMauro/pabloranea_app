9697733afbe9c21db7a859f49ee969e7c868b5ad
Venticinque Mauro <102001296+VenticinqueMauro@users.noreply.github.com>
2024-08-01 18:37:23 -0300

    add pdf en /travelmendoza

---
+++ src/app/[lang]/travelmendoza/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    metadataBase: new URL('https://pabloranea.com/'),
+    title: {
+        default: 'ITINERARY FEBRUARY / 2025',
+        template: '%s - Pablo Ranea',
+    },
+    description: 'Transportation according to itinerary for all round trips from the hotel. | Visits, tastings, and lunches in wineries mentioned. | Personalized online attention from 08:00 am to 10:00 pm and face-to-face in our office, located in the lobby at the selected hotel.'
+};
+
+export default async function PressLayout({ children }: { children: React.ReactNode }) {
+
+    return (
+        <section className='lg:pt-36'>
+            {children}
+        </section>
+    )
+}
---
+++ src/app/[lang]/travelmendoza/page.tsx
@@ -0,0 +1,9 @@
+
+export default function page() {
+
+    return (
+        <div style={{ width: '100%', height: '100vh' }}>
+            <embed src='/pdf/mvam_ItinerarioRaneaOK.pdf' type="application/pdf" width="100%" height="100%" />
+        </div>
+    )
+}
