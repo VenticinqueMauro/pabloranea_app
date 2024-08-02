9c64fe80e7b875f5923a8760066c7948fa8cda33
Venticinque Mauro <102001296+VenticinqueMauro@users.noreply.github.com>
2024-08-02 08:14:55 -0300

    change urls instagrams posts

--- src/components/instagram/EmbedCards.tsx
+++ src/components/instagram/EmbedCards.tsx
@@ -3,9 +3,9 @@ import { InstagramEmbed } from "react-social-media-embed";
 export default function EmbedCards() {
     return (
         <div className='flex flex-col items-center lg:justify-center lg:flex-row gap-4'>
-            <InstagramEmbed url="https://www.instagram.com/reel/C5o4wHIuZaB/" width={328} height={615} />
-            <InstagramEmbed url="https://www.instagram.com/reel/C7mqPkVOHXi/" width={328} height={615} />
-            <InstagramEmbed url="https://www.instagram.com/reel/C7h1kanu3KL/" width={328} height={615} />
+            <InstagramEmbed url="https://www.instagram.com/p/C92cL8uuqCA/" width={328} height={615} />
+            <InstagramEmbed url="https://www.instagram.com/p/C9x5ZUYupIN/" width={328} height={615} />
+            <InstagramEmbed url="https://www.instagram.com/reel/C8uaiDKunGR/" width={328} height={615} />
         </div>
     )
 }
