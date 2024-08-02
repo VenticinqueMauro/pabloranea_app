26f77f58698c32fa48de05b107f58e16de95ff37
Venticinque Mauro <102001296+VenticinqueMauro@users.noreply.github.com>
2024-08-02 08:40:54 -0300

    change pdf file in travelmendoza

--- src/app/[lang]/travelmendoza/page.tsx
+++ src/app/[lang]/travelmendoza/page.tsx
@@ -2,7 +2,7 @@ export default function Page() {
     return (
         <div style={{ width: '100%', height: '100vh' }}>
             <iframe
-                src='https://drive.google.com/file/d/1GfC5fOZnWYp1I1UK_houwEOe8OfMIbsP/preview'
+                src='https://drive.google.com/file/d/1oNUAmGnA6LYdDxY99G7qqf9NhbeKCu79/preview'
                 width="100%"
                 height="100%"
                 style={{ border: 'none' }}
