23729ecdf04bfe77ef04ec1ef7c45b076c816484
Venticinque Mauro <102001296+VenticinqueMauro@users.noreply.github.com>
2026-01-08 13:43:57 -0300

    update instagram post

--- src/components/instagram/EmbedCards.tsx
+++ src/components/instagram/EmbedCards.tsx
@@ -4,8 +4,8 @@ export default function EmbedCards() {
     return (
         <div className='flex flex-col gap-4 items-center lg:justify-center lg:flex-row'>
             <InstagramEmbed url="https://www.instagram.com/reel/DJ9vZAyuYuN/?utm_source=ig_web_copy_link&igsh=MTRkcDZ1NGN6ZnZ6cA%3D%3D" width={328} height={615} />
-            <InstagramEmbed url="https://www.instagram.com/reel/DOMvpYIDvOh/?utm_source=ig_web_copy_link&igsh=N3dnYmEzZ3p0djAz" width={328} height={615} />
-            <InstagramEmbed url="https://www.instagram.com/reel/DOTmGWXjrdv/?utm_source=ig_web_copy_link&igsh=ZnU5ajNpdTM3bXB0" width={328} height={615} />
+            <InstagramEmbed url="https://www.instagram.com/reel/DS7tWHyjqz_/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" width={328} height={615} />
+            <InstagramEmbed url="https://www.instagram.com/p/DQ-YLS8DibX/" width={328} height={615} />
         </div>
     )
 }
