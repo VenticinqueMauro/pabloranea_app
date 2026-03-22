fd5e0b283254d384b6499d21ee94e43f9ff5e3af
Venticinque Mauro <102001296+VenticinqueMauro@users.noreply.github.com>
2026-03-21 22:25:33 -0300

    Add deployment comment in HomePage function
    Added a comment for deployment reference.

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -2,6 +2,7 @@ import HomeContent from "@/components/HomeContent";
 import { Lang } from "@/types/lang.type";
 
 export default async function HomePage({ params: { lang } }: Lang) {
+  //deploy
   const dictionary = await import(`@/app/dictionaries/${lang}.json`).then(m => m.default);
 
   return <HomeContent lang={lang} dictionary={dictionary} />;
