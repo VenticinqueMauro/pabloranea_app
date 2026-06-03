e90942f1bfb744a575c6e237d0a30978e458e770
Mauro Venticinque <mauro.venticinque@hotmail.com>
2026-06-03 11:30:02 -0300

    Update Instagram embeds with new posts in canonical URL format
      - Replace the 3 Instagram embeds in EmbedCards with new posts (1 post
        + 2 reels), in the requested order
      - Use canonical instagram.com/p|reel/CODE URLs (without the
        /pabloranea username segment) so the official embed.js renders the
        content instead of staying on the 'View post on
        Instagram' placeholder

--- src/components/instagram/EmbedCards.tsx
+++ src/components/instagram/EmbedCards.tsx
@@ -3,9 +3,9 @@ import { InstagramEmbed } from "react-social-media-embed";
 export default function EmbedCards() {
     return (
         <div className='flex flex-col gap-4 items-center lg:justify-center lg:flex-row'>
-            <InstagramEmbed url="https://www.instagram.com/reel/DJ9vZAyuYuN/?utm_source=ig_web_copy_link&igsh=MTRkcDZ1NGN6ZnZ6cA%3D%3D" width={328} height={615} />
-            <InstagramEmbed url="https://www.instagram.com/reel/DS7tWHyjqz_/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" width={328} height={615} />
-            <InstagramEmbed url="https://www.instagram.com/p/DQ-YLS8DibX/" width={328} height={615} />
+            <InstagramEmbed url="https://www.instagram.com/p/DY7nCOllEhH/" width={328} height={615} />
+            <InstagramEmbed url="https://www.instagram.com/reel/DYNXR5Mqr5K/" width={328} height={615} />
+            <InstagramEmbed url="https://www.instagram.com/reel/DY0s7ZzKY6k/" width={328} height={615} />
         </div>
     )
 }
